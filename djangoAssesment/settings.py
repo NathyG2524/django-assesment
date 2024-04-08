28a764915718fa74fcc7e8c196f042ea6d072fa5
NathyG <nathygirma2524@gmail.com>
2024-04-08 09:21:31 +0300

    feat(mereb-auth): pr)

--- djangoAssesment/settings.py
+++ djangoAssesment/settings.py
@@ -186,9 +186,7 @@ ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
+        
         'APP': {
             'client_id': GOOGLE_CLIENT_ID,
             'secret': GOOGLE_SECRET,
